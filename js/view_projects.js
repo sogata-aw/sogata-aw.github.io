7864861951e867d21548d1a05f66154aa30f5179
Sogata <maximer819@gmail.com>
2025-03-23 11:01:28 +0100

    Modification de la page des projets et de la navbar

---
+++ js/view_projects.js
@@ -0,0 +1,10 @@
+/**
+ * Objet constant représentant la vue.
+ */
+export const view = {
+
+  articles: document.querySelectorAll(".project_table"),
+
+  articleMoreInfo: document.getElementById("more_info"),
+
+};
