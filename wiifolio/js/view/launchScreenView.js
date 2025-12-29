57132cdcbdfd5d23d6b789746ae717074b955633
sogata-aw <75533495+sogata-aw@users.noreply.github.com>
2025-12-29 15:29:14 +0100

    Delete wiifolio directory

--- wiifolio/js/view/launchScreenView.js
+++
@@ -1,5 +0,0 @@
-export const view = {
-    body: document.querySelector("body"),
-    principale: document.querySelector(".principale"),
-    clickAudio : new Audio("../sounds/Sample_0002.wav"),
-}
--- wiifolio/js/view/menu-view.js
+++
@@ -1,3 +0,0 @@
-export const view = {
-    dateText: document.querySelector(".date"),
-}
