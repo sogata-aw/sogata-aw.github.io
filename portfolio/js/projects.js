b6f59d01fd3984c038468c18eb84a4aebb2c00d1
Sogata <maximer819@gmail.com>
2026-05-14 11:57:52 +0200

    Amélioration du css

--- portfolio/js/projects.js
+++ portfolio/js/projects.js
@@ -2,12 +2,47 @@ import { view } from './view_projects.js';
 
 let last_click = null;
 
-window.onload = async function(){
+window.onload = async function () {
     const more_info_text = await fetch("../projects_info.json").then(data => data.json());
     const projects = await fetch("../projects.json").then(data => data.json());
 
     const projectTemplate = document.getElementById("project");
     const infoTemplate = document.getElementById("info");
+
+    function loadInfos(id) {
+        const clone = document.importNode(infoTemplate.content, true);
+
+        const objectif = clone.querySelector("#objectif");
+        objectif.innerHTML = more_info_text[id].objectif;
+
+        const texte1 = clone.querySelector("#texte1");
+        texte1.innerHTML = more_info_text[id].texte1;
+
+        if (more_info_text[id].texte2) {
+            const p = clone.querySelector("#texte2");
+            p.innerHTML = more_info_text[id].texte2
+        }
+
+        const ul = clone.querySelector("#links_list");
+
+        if (more_info_text[id].liens.length == 0) {
+            const links = clone.querySelector("#links");
+            links.remove();
+            ul.remove();
+        } else {
+            more_info_text[id].liens.forEach(lien => {
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.href = lien.url;
+                a.innerHTML = lien.nom;
+                li.appendChild(a);
+                ul.appendChild(li);
+            });
+        }
+
+        view.articleMoreInfo.appendChild(clone);
+    }
+
 
     projects.forEach(project => {
         const clone = document.importNode(projectTemplate.content, true);
@@ -25,63 +60,44 @@ window.onload = async function(){
         view.projects.appendChild(clone);
     });
 
+    const url = window.location.hash.substring(1);
+    if (url) {
+        last_click = url;
+        view.articleMoreInfo.classList.remove("hidden");
+        view.articleMoreInfo.classList.add("visible");
+        loadInfos(last_click);
+        view.articleMoreInfo.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+    }
+
+
     view.articles = document.querySelectorAll(".project_table")
 
     view.articles.forEach((article) => {
-    article.addEventListener("click", function () {
-        if(last_click == article.id){
-            view.articleMoreInfo.classList.remove("visible");
-            view.articleMoreInfo.classList.add("hidden");
-        }else{
-            if(!view.articleMoreInfo.classList.contains("visible")){
-                view.articleMoreInfo.classList.remove("hidden");
-                view.articleMoreInfo.classList.add("visible");
-            };
+        article.addEventListener("click", function () {
+            if (last_click == article.id) {
+                view.articleMoreInfo.classList.remove("visible");
+                view.articleMoreInfo.classList.add("hidden");
+            } else {
+                if (!view.articleMoreInfo.classList.contains("visible")) {
+                    view.articleMoreInfo.classList.remove("hidden");
+                    view.articleMoreInfo.classList.add("visible");
+                };
 
-            while(view.articleMoreInfo.firstChild){
-            view.articleMoreInfo.removeChild(view.articleMoreInfo.firstChild);
-        }
+                while (view.articleMoreInfo.firstChild) {
+                    view.articleMoreInfo.removeChild(view.articleMoreInfo.firstChild);
+                }
 
-            const clone = document.importNode(infoTemplate.content, true);
+                loadInfos(article.id);
 
-            const objectif = clone.querySelector("#objectif");
-            objectif.innerHTML = more_info_text[article.id].objectif;
-
-            const texte1 = clone.querySelector("#texte1");
-            texte1.innerHTML = more_info_text[article.id].texte1;
-
-            if(more_info_text[article.id].texte2){
-                const p = clone.querySelector("#texte2");
-                p.innerHTML = more_info_text[article.id].texte2
-            }
-
-            const ul = clone.querySelector("#links_list");
-
-            if(more_info_text[article.id].liens.length == 0){
-                const links = clone.querySelector("#links");
-                links.remove();
-                ul.remove();
-            }else{
-                more_info_text[article.id].liens.forEach(lien => {
-                    const li = document.createElement("li");
-                    const a = document.createElement("a");
-                    a.href = lien.url;
-                    a.innerHTML = lien.nom;
-                    li.appendChild(a);
-                    ul.appendChild(li);
+                last_click = article.id;
+                view.articleMoreInfo.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
                 });
             }
-
-            
-
-            view.articleMoreInfo.appendChild(clone);
-
-            last_click = article.id;
-            view.articleMoreInfo.scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        }
-    })
-});
+        })
+    });
 }
